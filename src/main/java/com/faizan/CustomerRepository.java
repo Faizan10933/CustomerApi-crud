16411b233225a44646ee5a98536743d678866781
Faizan <faizan10933@gmail.com>
2023-07-03 23:00:38 +0530

    crud api

---
+++ src/main/java/com/faizan/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.faizan;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository
+        extends JpaRepository<Customer, Integer> {
+
+
+}
